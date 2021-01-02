c4a1d62bd9a1bd2ef3aeab28d02581ca156aef58
cjh8817676 <jonathan1446171@gmail.com>
2021-01-02 23:00:26 +0800

    測試

--- routes/contentInstance.js
+++ routes/contentInstance.js
@@ -10,6 +10,7 @@ router.get('/', function(req, res, next) {
 /* POST users listing. */
 router.post('/', function(req, res, next) {
   console.log(req.body);
+  console.log('haha');
   creat_DESCRIPTOR_contentInstance(req.body);
   res.send('received data='+req.body.mydata2);
 });
@@ -32,6 +33,7 @@ function creat_DESCRIPTOR_contentInstance(input){
               &lt;str name=&quot;End_Time&quot; val=&quot;${input.End_Time}&quot;/&gt;
               &lt;str name=&quot;Weight&quot; val=&quot;${input.Weight}&quot;/&gt;
               &lt;str name=&quot;number_of_set&quot; val=&quot;${input.number_of_set}&quot;/&gt;
+              &lt;str name=&quot;Average_Speed&quot; val=&quot;${input.Average_Speed}&quot;/&gt;
               &lt;str name=&quot;Calories&quot; val=&quot;${input.Calories}&quot;/&gt;
               &lt;op name=&quot;getValue&quot; href=&quot;/in-cse/in-name/MY_SENSOR/DATA/la&quot;
            in=&quot;obix:Nil&quot; out=&quot;obix:Nil&quot; is=&quot;retrieve&quot;/&gt;
