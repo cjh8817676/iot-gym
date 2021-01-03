d30a2eb000d0aadafaa9dfb71995f50cc9769272
106360130 <bs25017@gmail.com>
2021-01-03 23:11:32 +0800

    可以將.txt的字串分解 且 存在object

--- routes/read_txt.js
+++ routes/read_txt.js
@@ -9,42 +9,57 @@ fs.readFile('D://JeffTaipeiTech2020//i_GYM//read_txt.txt', function (error, data
     }
     //console.log(data)
     var data_str;
-    data_str = data.toString();
-    var data_str_type = typeof(data_str);
+    data_str = data.toString();  //將型別轉成string
+    //var data_str_type = typeof(data_str);
+    //console.log('data_str : ' + data_str)
+    //console.log('data_str_type : ' + data_str_type)
 
-    console.log('data_str : ' + data_str)
+    //console.log('data : ' + data.toString())  //將檔案以字串讀出
+    //console.log('data\'length : ' + data.length)
+    //var data_type = typeof(data)
+    //console.log('data : ' + data_type)
+    /*
+    var string_temp = '123\n456\n77\n\n789787\n445644\n22\n\n';
+    var str_split = string_temp.split("\n\n")
+    console.log('str_split : ' + str_split)
+    console.log(str_split[0]);
+
+    var str_split_2 = str_split[0];
+    console.log('str_split_2 : ' + str_split_2);
+    var str_split_3 = str_split_2.split("\n");
+    console.log('str_split_3 : ' + str_split_3);
+   
+
+    console.log(typeof(str_split[0]))
+     */
+
+    var data_str_split = data_str.split("\n\n");
+    console.log('data_str_split : ' + data_str_split);
+    var data_str_split_temp = data_str_split[0];
+    data_str_split_2 = data_str_split_temp.split("\n");
+
+    var input = {};
+    input.User_Name = data_str_split_2[0];
+    input.Start_Time = data_str_split_2[1];
+    input.End_Time = data_str_split_2[2];
+    input.Weight = data_str_split_2[3];
+    input.Reps = data_str_split_2[4];
+    input.number_of_set = data_str_split_2[5];
+    input.Average_speed = data_str_split_2[6];
+    input.Calories = data_str_split_2[7];
+
+    console.log('User_Name : ' + input.User_Name);
+    console.log('Start_Time : ' + input.Start_Time);
+    console.log('End_Time : ' + input.End_Time);
+    console.log('Weight : ' + input.Weight);
+    console.log('Reps : ' + input.Reps);
+    console.log('number_of_set : ' + input.number_of_set);
+    console.log('Average_speed : ' + input.Average_speed);
+    console.log('Calories : ' + input.Calories);
 
 
-    console.log('data_str_type : ' + data_str_type)
-    console.log('data : ' + data.toString())  //將檔案以字串讀出
-    console.log('data\'length : ' + data.length)  //長度會多兩個字元
-    var data_type = typeof(data)
-    console.log('data : ' + data_type)
-    var string_temp = '123 123';
-    var str_temp_type = typeof(string_temp)
-    console.log('str_temp : ' + str_temp_type)
 
-    for(var i = 0; i < data.length; i++)
-    {
-        console.log(data[i])
-        string_temp[i] = data[i]
-        console.log('i : ' + i);
-        string_temp.split(" ")
-        //console.log(string_temp[i])
-        /*
-        if(data[i] == 32)
-        {
-            break;
-        }
-        else
-        {
-            console.log(data[i])
-            string_temp[i] = data[i]
-        }
-        */
-    }
-    
-    console.log('string_temp : ' + string_temp)
+
 
   })
 
