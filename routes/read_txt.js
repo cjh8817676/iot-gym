9678fe4d0adc41d9a1d8fd5dfbd4e95301cca471
106360130 <bs25017@gmail.com>
2021-01-04 14:17:41 +0800

    讀檔副程式寫完，正要匯入至其他js檔

--- routes/read_txt.js
+++ routes/read_txt.js
@@ -1,44 +1,37 @@
 var fs = require('fs');  //呼叫副程式"file system"
 
 
-fs.readFile('D://JeffTaipeiTech2020//i_GYM//read_txt.txt', function (error, data) {
+//非同步讀取資料
+/*
+fs.readFile('D://JeffTaipeiTech2020//i_GYM//read_txt.txt', "utf8", function(error, data) {
     // 若錯誤 error 為一個物件，則會在這邊觸發內部程式碼，作為簡單的錯誤處理
+    
     if (error) {
         console.log('讀取檔案失敗')
         return
     }
+    
+   
     //console.log(data)
     var data_str;
     data_str = data.toString();  //將型別轉成string
-    //var data_str_type = typeof(data_str);
-    //console.log('data_str : ' + data_str)
-    //console.log('data_str_type : ' + data_str_type)
+    //console.log(data_str)
+    //console.log(typeof(data_str))
+    read_data = data_str;
+    
+    var data_str;
+    data_str = data.toString();  //將型別轉成string
 
-    //console.log('data : ' + data.toString())  //將檔案以字串讀出
-    //console.log('data\'length : ' + data.length)
-    //var data_type = typeof(data)
-    //console.log('data : ' + data_type)
-    /*
-    var string_temp = '123\n456\n77\n\n789787\n445644\n22\n\n';
-    var str_split = string_temp.split("\n\n")
-    console.log('str_split : ' + str_split)
-    console.log(str_split[0]);
 
-    var str_split_2 = str_split[0];
-    console.log('str_split_2 : ' + str_split_2);
-    var str_split_3 = str_split_2.split("\n");
-    console.log('str_split_3 : ' + str_split_3);
-   
-
-    console.log(typeof(str_split[0]))
-     */
-
+    //將object字串分解
     var data_str_split = data_str.split("\n\n");
-    console.log('data_str_split : ' + data_str_split);
+    //console.log('data_str_split : ' + data_str_split);
     var data_str_split_temp = data_str_split[0];
     data_str_split_2 = data_str_split_temp.split("\n");
+    //將object字串分解
 
-    var input = {};
+    //將分解的字串放進對的位置
+    var input = {};  //宣告物件
     input.User_Name = data_str_split_2[0];
     input.Start_Time = data_str_split_2[1];
     input.End_Time = data_str_split_2[2];
@@ -47,6 +40,7 @@ fs.readFile('D://JeffTaipeiTech2020//i_GYM//read_txt.txt', function (error, data
     input.number_of_set = data_str_split_2[5];
     input.Average_speed = data_str_split_2[6];
     input.Calories = data_str_split_2[7];
+    
 
     console.log('User_Name : ' + input.User_Name);
     console.log('Start_Time : ' + input.Start_Time);
@@ -56,11 +50,92 @@ fs.readFile('D://JeffTaipeiTech2020//i_GYM//read_txt.txt', function (error, data
     console.log('number_of_set : ' + input.number_of_set);
     console.log('Average_speed : ' + input.Average_speed);
     console.log('Calories : ' + input.Calories);
+    //將分解的字串放進對的位置
+
+});
+*/
+//非同步讀取資料
+
+//同步讀取資料
+var read_data = fs.readFileSync('D://JeffTaipeiTech2020//i_GYM//read_txt.txt');
+//console.log('data : ' + read_data);
+//同步讀取資料
 
 
+//分解data
+function split_data(read_data)
+{
+    //console.log(data)
+    var data_str;
+    data_str = read_data.toString();  //將型別轉成string
+    //console.log(data_str)
+    //console.log(typeof(data_str))
+    read_data = data_str;
 
+    //將object分解
+    var data_str_split = data_str.split("\n\n");
+    //console.log('data_str_split : ' + data_str_split);
+    //將object字串分解
 
+    return data_str_split;
+}
+//分解data
 
-  })
+//分解字串
+function data_content(data_str_split)
+{
+    //將object字串分解
+    var data_str_split_temp = data_str_split;
+    data_str_split_2 = data_str_split_temp.split("\n");
+    //將object字串分解
 
+    //將分解的字串放進對的位置
+    var input = {};  //宣告物件
+    input.User_Name = data_str_split_2[0];
+    input.Start_Time = data_str_split_2[1];
+    input.End_Time = data_str_split_2[2];
+    input.Weight = data_str_split_2[3];
+    input.Reps = data_str_split_2[4];
+    input.number_of_set = data_str_split_2[5];
+    input.Average_speed = data_str_split_2[6];
+    input.Calories = data_str_split_2[7];
+    
+    /*
+    console.log('User_Name : ' + input.User_Name);
+    console.log('Start_Time : ' + input.Start_Time);
+    console.log('End_Time : ' + input.End_Time);
+    console.log('Weight : ' + input.Weight);
+    console.log('Reps : ' + input.Reps);
+    console.log('number_of_set : ' + input.number_of_set);
+    console.log('Average_speed : ' + input.Average_speed);
+    console.log('Calories : ' + input.Calories);
+    */
+    //將分解的字串放進對的位置
+
+    console.log('input' + input);
+
+    return input
+}
+
+var data_str_split = split_data(read_data);
+var input = data_content(data_str_split[0]);
+
+/*
+console.log('User_Name : ' + input.User_Name);
+console.log('Start_Time : ' + input.Start_Time);
+console.log('End_Time : ' + input.End_Time);
+console.log('Weight : ' + input.Weight);
+console.log('Reps : ' + input.Reps);
+console.log('number_of_set : ' + input.number_of_set);
+console.log('Average_speed : ' + input.Average_speed);
+console.log('Calories : ' + input.Calories);
+*/
   
+
+//module.exports = router;
+/*
+module.exports = {
+    split_data : split_data,
+    data_content : data_content
+};
+*/
--- routes/tempCodeRunnerFile.js
+++ routes/tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-
-    headers = {
+read_data
